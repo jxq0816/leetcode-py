c8abf35dddd7d9bfcaf67e66be79d3a5349d611a
姜兴琪 <xingqijiang@qq.com>
2021-09-18 00:14:43 +0800

    No.73 矩阵置零

---
+++ no73_setZeroes.py
@@ -0,0 +1,21 @@
+from typing import List
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        m, n = len(matrix), len(matrix[0])
+        row, col = [False] * m, [False] * n
+
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] == 0:
+                    row[i] = col[j] = True
+
+        for i in range(m):
+            for j in range(n):
+                if row[i] or col[j]:
+                    matrix[i][j] = 0
+
+if __name__ == '__main__':
+    matrix = [[1,1,1],[1,0,1],[1,1,1]]
+    solution=Solution()
+    solution.setZeroes(matrix)
+    print(matrix)
